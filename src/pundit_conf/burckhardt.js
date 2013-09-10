9264ae35ed878876378c142dfcd2de198167f9b0
Simone Fonda <fonda@netseven.it>
2013-09-10 14:56:42 +0200

    Removed EuropeanaEDM selector from burck conf

--- src/pundit_conf/burckhardt.js
+++ src/pundit_conf/burckhardt.js
@@ -56,6 +56,9 @@ var punditConfig = {
         },
         
         'selectors': {
+            'EuropeanaEDM': {
+                active: false
+            }
             /*
             'KorboBasket': {
                 name: 'korbo', 
