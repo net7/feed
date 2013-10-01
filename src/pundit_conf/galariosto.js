f3e48075a31c295bfda4424a7bced8d9d071de5b
Simone Fonda <fonda@netseven.it>
2013-10-01 11:06:20 +0200

    Added galassia ariosto configuration and introduction file

---
+++ src/pundit_conf/galariosto.js
@@ -0,0 +1,60 @@
+var punditConfig = {
+
+    debugAllModules: false,
+
+    annotationServerBaseURL: "http://demo.as.thepund.it:8080/annotationserver/",
+
+    vocabularies: [
+        "http://manager.korbo.org/103?jsonp=_PUNDIT.vocab.initJsonpVocab",
+        "http://manager.korbo.org/112?jsonp=_PUNDIT.vocab.initJsonpVocab"
+    ],
+    
+
+    useBasicRelations: false,
+
+    modules: {
+
+        'pundit.Help': {
+            introductionFile: '/introductions/intro-galariosto.html',
+            introductionWindowTitle: "Welcome to Galassia Ariosto's Pundit!",
+            showIntroductionAtLogin: true
+        },
+
+        'pundit.ContactHelper': {
+            active: false
+        },
+
+        'pundit.NamedContentHandler': {
+            active: false
+        },
+        'pundit.NotebookManager': {
+            active: true
+        },
+        'pundit.ImageFragmentHandler': {
+            active: false
+        },
+        'pundit.ImageAnnotationPanel': {
+            active: false
+        },
+        'pundit.PageHandler': {
+            active: false
+        },
+        'pundit.Recognizer': {
+            active: false
+        },
+        'pundit.CommentTagPanel': {
+            active: true,
+            enableEntitiesExtraction: false
+        },
+        
+        
+        'selectors': {
+            'EuropeanaEDM': {
+                active: false
+            }
+        },
+        
+        'annotators': {}
+    }
+
+};
