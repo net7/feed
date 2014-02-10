1e837f0c59cc2f94562d188fd23956f9086f8415
Romeo Zitarosa <zitarosa@netseven.it>
2014-02-10 16:02:40 +0100

    fissata conf per galariosto

--- src/pundit_conf/galariosto.js
+++ src/pundit_conf/galariosto.js
@@ -57,28 +57,28 @@ var punditConfig = {
                     queryType: 'http://purl.org/galassiariosto/types/Azione',
                     name: 'murucaazioni',
                     label: 'Azioni',
-                    MurucaReconURL: "http://demo2.galassiaariosto.netseven.it/reconcile",
+                    MurucaReconURL: "http://galassiaariosto.netseven.it/reconcile",
                     active: true
                 },
                 {
                     queryType: 'http://purl.org/galassiariosto/types/Scena',
                     name: 'murucascene',
                     label: 'Scene',
-                    MurucaReconURL: "http://demo2.galassiaariosto.netseven.it/reconcile",
+                    MurucaReconURL: "http://galassiaariosto.netseven.it/reconcile",
                     active: true
                 },
                 {
                     queryType: 'http://purl.org/galassiariosto/types/Ecphrasis',
                     name: 'murucaecphrasis',
                     label: 'Ecphrasis',
-                    MurucaReconURL: "http://demo2.galassiaariosto.netseven.it/reconcile",
+                    MurucaReconURL: "http://galassiaariosto.netseven.it/reconcile",
                     active: true
                 },
                 {
                     queryType: 'http://purl.org/galassiariosto/types/Paratesto',
                     name: 'murucaparatesto',
                     label: 'Paratesto',
-                    murucaReconURL: "http://demo2.galassiaariosto.netseven.it/reconcile",
+                    murucaReconURL: "http://galassiaariosto.netseven.it/reconcile",
                     active: true
                 }
             ]
