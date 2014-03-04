9171945cc72f55dc9e4f9252775093713eafa544
Christian Morbidoni <christian.morbidoni@gmail.com>
2014-03-04 11:33:25 +0100

    fixed netx prev bugs

--- src/partial/renderPunditContent.php
+++ src/partial/renderPunditContent.php
@@ -5,12 +5,12 @@ function renderPunditContent ($s, $pos = null) {
 ?>
     <div class="feed-header pundit-disable-annotation">
 
-        <?php if ($s->type=='Page' && $prev = $s->getPrev($pos)) { ?>
+        <?php if ($prev = $s->getPrev($pos)) { ?>
             <span class="label label-info prev">
                 <a href="<?php echo $prev ?>"><i class="icon-chevron-left"></i>Previous</a>
             </span>
         <?php } ?>
-        <?php if ($s->type=='Page' && $book = $s->getBookLink()) { ?>
+        <?php if ($book = $s->getBookLink()) { ?>
             <span class="label label-important prev">
                 <a href="<?php echo $book ?>"><i class="icon-chevron-up"></i>Entire work</a>
             </span>
@@ -20,7 +20,7 @@ function renderPunditContent ($s, $pos = null) {
                 <a href="#" rel="popover" title="<?php echo $s->getDomain() ?>" data-placement="right" data-content="<?php echo $s->getComment() ?>"><i class="icon-info-sign"></i></a>
             </span>
 
-        <?php if ($s->type=='Page' && $next = $s->getNext($pos)) { ?>
+        <?php if ($next = $s->getNext($pos)) { ?>
             <span class="label label-info next">
                 <a href="<?php echo $next ?>">Next <i class=" icon-chevron-right"></i></a>
             </span>
