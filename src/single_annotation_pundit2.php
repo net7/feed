87a7c4024666bfcd610355c33a4fc7bf01bd35c0
piccioli <piccioli@netseven.it>
2014-06-20 22:31:09 +0200

    Added pundit2 and API ?url2 - EXPERIMENTAL BUT WORKING

---
+++ src/single_annotation_pundit2.php
@@ -0,0 +1,36 @@
+<?php
+try {
+$s=new Scraper($_GET['url2']);
+}
+ catch (Exception $e) {
+     echo $e->getMessage();
+     die();
+ }
+ 
+?>
+<!doctype html>
+<html>
+    <head>
+        <?php if ($css = $s->getStylesheet()) { ?>
+            <link rel="stylesheet" href="<?php echo $css; ?>" type="text/css">
+        <?php } ?>
+        
+        <link rel="stylesheet" href="css/feed.css" type="text/css">        
+        
+        <link rel="stylesheet" href="pundit2/pundit2.css" type="text/css">        
+        <script src="pundit2/libs.js" type="text/javascript" ></script>
+        <script src="pundit2/pundit2.js" type="text/javascript" ></script>
+        <script>var punditConfig = {
+                modules: {
+                    'Client': {
+                        active: true
+                    }
+                }
+            }</script>
+        
+    </head>
+    <body data-ng-app="Pundit2" >
+        <?php renderPunditContent($s) ; ?>
+        <?php renderFooter(); ?>
+    </body>
+</html>
