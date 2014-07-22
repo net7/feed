123f384519f3dc1cf2ee061eabb287abdaf40fed
piccioli <piccioli@netseven.it>
2014-07-22 15:56:03 +0200

    added conf parameter to set a specific pundit2 configuration by its URL

--- src/single_annotation_pundit2.php
+++ src/single_annotation_pundit2.php
@@ -23,8 +23,16 @@ $s=new Scraper($_GET['url2'],'advanced');
         <link rel="stylesheet" href="http://dev.thepund.it/download/client/last-beta/pundit2.css" type="text/css">        
         <script src="http://dev.thepund.it/download/client/last-beta/libs.js" type="text/javascript" ></script>
         <script src="http://dev.thepund.it/download/client/last-beta/pundit2.js" type="text/javascript" ></script>
+
+<?php if(isset($_GET['conf'])): ?>
+
+        <script src="<?php echo $_GET['conf'] ?>" type="text/javascript" ></script>
+
+      <?php else: ?>
+
         <script src="http://dev.thepund.it/download/client/last-beta/pundit2_conf.js" type="text/javascript" ></script>
-        
+
+      <?php endif; ?>        
     </head>
     <body data-ng-app="Pundit2" >
         <?php renderPunditContent($s) ; ?>
